27c43799a0f54a6ec2e58e049995294d1945b340
Siddarth Senthilkumar <siddarthsenthilkumar@gmail.com>
2018-11-08 15:18:52 -0800

    Added Kevin's suggested fixes

--- src/lib/DatablockFreelist.h
+++ src/lib/DatablockFreelist.h
@@ -6,23 +6,23 @@
 #include "BlockManager.h"
 #include "Superblock.h"
 
-#define NUM_FREE_BLOCKS (4096 / sizeof(uint64_t) - 2)
-
 struct DatablockNode {
-  uint64_t prev_block = 0;
-  uint64_t next_block = 0;
-  uint64_t free_blocks[NUM_FREE_BLOCKS];
+  static const int NREFS = (4096 / sizeof(uint64_t) - 2);
+
+  Block::ID prev_block = 0;
+  Block::ID next_block = 0;
+  Block::ID free_blocks[NREFS];
 };
 
 class DatablockFreelist {
-  uint64_t top_block_num;
+  Block::ID top_block_num;
   uint64_t index;
   BlockManager* disk;
 public:
-  DatablockFreelist(uint64_t top_block_num, uint64_t index, BlockManager& disk);
+  DatablockFreelist(Block::ID top_block_num, uint64_t index, BlockManager& disk);
   ~DatablockFreelist();
-  virtual void insert(uint64_t block_number);
-  virtual uint64_t remove();
+  virtual void insert(Block::ID block_number);
+  virtual Block::ID remove();
 };
 
 #endif
