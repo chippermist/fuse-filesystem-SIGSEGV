b2da8d6388f87dcd1c5f1c8cdeb90bf2e7553bc6
Chinmay Garg <chinmaygarg@me.com>
2018-11-09 16:19:53 -0800

    Revert "More weird type hacks."
    This reverts commit 937f24951fa18bf97c2126c6fad33a99fdd5a9df.

--- src/lib/Filesystem.h
+++ src/lib/Filesystem.h
@@ -20,8 +20,8 @@ public:
   // FUSE Operations:
   int chmod(const char*, mode_t);
   int chown(const char*, uid_t, gid_t);
-  int flush(const char*, struct fuse_file_info*);
-  int fsync(const char*, int, struct fuse_file_info*);
+  int flush(const char*, fuse_file_info*);
+  int fsync(const char*, int, fuse_file_info*);
   int getattr(const char*, struct stat*);
   int getdir(const char*, fuse_dirh_t, fuse_dirfil_t);
   int getxattr(const char*, const char*, char*, size_t);
@@ -29,10 +29,10 @@ public:
   int listxattr(const char*, char*, size_t);
   int mkdir(const char*, mode_t);
   int mknod(const char*, mode_t, dev_t);
-  int open(const char*, struct fuse_file_info*);
-  int read(const char*, char*, size_t, off_t, struct fuse_file_info*);
+  int open(const char*, fuse_file_info*);
+  int read(const char*, char*, size_t, off_t, fuse_file_info*);
   int readlink(const char*, char*, size_t);
-  int release(const char*, struct fuse_file_info*);
+  int release(const char*, fuse_file_info*);
   int removexattr(const char*, const char*);
   int rename(const char*, const char*);
   int rmdir(const char*);
@@ -42,7 +42,7 @@ public:
   int truncate(const char*, off_t);
   int unlink(const char*);
   int utime(const char*, utimbuf*);
-  int write(const char*, const char*, size_t, off_t, struct fuse_file_info*);
+  int write(const char*, const char*, size_t, off_t, fuse_file_info*);
 };
 
 #endif
