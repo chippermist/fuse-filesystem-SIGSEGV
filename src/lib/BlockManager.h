7308b01d2c40084c1031dc5177fc63e94de61236
Chinmay Garg <chinmaygarg@me.com>
2018-11-14 21:53:55 -0800

    fixed compilation bugs

--- src/lib/BlockManager.h
+++ src/lib/BlockManager.h
@@ -6,12 +6,6 @@ class BlockManager {
 public:
   BlockManager(Block::ID top_block_num, uint64_t index, Storage& disk);
   ~BlockManager();
-
-  virtual void mkfs() = 0;
-  virtual void insert(Block::ID block_number) = 0;
-  virtual Block::ID remove() = 0;
-};
-
   virtual void mkfs() = 0;
   virtual void insert(Block::ID block_number) = 0;
   virtual Block::ID remove() = 0;
