530e97fc5327feaa0f717cbe802fefa614dabe4b
Chinmay Garg <chinmaygarg@me.com>
2018-11-15 19:31:53 -0800

    made Storage& into a struct to resolve error

--- src/lib/BlockManager.h
+++ src/lib/BlockManager.h
@@ -4,7 +4,7 @@
 
 class BlockManager {
 public:
-  BlockManager(Block::ID top_block_num, uint64_t index, Storage& disk);
+  BlockManager(Block::ID top_block_num, uint64_t index, struct Storage& disk);
   ~BlockManager();
   virtual void mkfs() = 0;
   virtual void insert(Block::ID block_number) = 0;
