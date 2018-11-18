37ce0c64f166891c798530a20020512e16fdd1f8
Siddarth Senthilkumar <siddarthsenthilkumar@gmail.com>
2018-11-17 22:32:01 -0800

    Created read() and write()

--- src/lib/FileAccessManager.cpp
+++ src/lib/FileAccessManager.cpp
@@ -1,6 +1,7 @@
 #include "FileAccessManager.h"
 
-FileAccessManager::FileAccessManager(INodeManager& inode_manager, Storage& storage) {
+FileAccessManager::FileAccessManager(BlockManager& block_manager, INodeManager& inode_manager, Storage& storage) {
+  this->block_manager = &block_manager;
   this->inode_manager = &inode_manager;
   this->disk = &storage;
 }
@@ -9,21 +10,360 @@ FileAccessManager::~FileAccessManager() {
   // Nothing to do.
 }
 
+/**
+ * Writes size bytes from buf into a file, starting at the given offset.
+ *
+ * Inserts NULL filler if the starting offset is beyond the file's size,
+ * and this affects the returned total bytes written.
+ *
+ * Returns -1 on error.
+ * - No such file
+ * - Gave name of directory
+ * - TODO: Incorrect ownership/permissions
+ */
+size_t FileAccessManager::write(std::string path, char *buf, size_t size, size_t offset) {
+
+  // Read the file's inode and do some sanity checks
+  INode::ID file_inode_num = getINodeFromPath(path);
+  if (file_inode_num == 0) {
+    return -1; // File not found
+  }
+  INode file_inode;
+  this->inode_manager->get(file_inode_num, file_inode);
+
+  if (file_inode.type != FileType::REGULAR) {
+    return -1; // File is not a regular file
+  }
+
+  // TODO: Check ownership and permissions
+
+  size_t total_written = 0;
+  // 1. If we are overwriting any data in the file, do that first.
+  while (offset < file_inode.size && size > 0) {
+
+    // Read in block
+    Block block;
+    Block::ID block_num = blockAt(file_inode, offset);
+    this->disk->get(block_num, block);
+
+    /*
+      How many bytes to write?
+      a) Normally write whole blocks at a time.
+      b) If the offset isn't block aligned though
+         (which could happen for the first block),
+         then only write until the end of the block.
+      c) Don't write past the end of the file (handled in step 3).
+      d) Of course, don't write more than what was asked!
+    */
+    size_t to_write = Block::SIZE - (offset % Block::SIZE);
+
+    if (offset + to_write > file_inode.size) {
+      to_write = file_inode.size - offset;
+    }
+
+    if (to_write > size) {
+      to_write = size;
+    }
+
+    // Copy the data and write to disk
+    memcpy(block.data + (offset % Block::SIZE), buf, to_write);
+    this->disk->set(block_num, block);
+
+    // Update offset, buf pointer, and num bytes left to write
+    offset += to_write;
+    buf += to_write;
+    size -= to_write;
+
+    total_written += to_write;
+  }
+
+  if (size <= 0) {
+    assert(size == 0);
+    return total_written;
+  }
+
+  // 2. If the offset > file size, insert NULL filler.
+  size_t null_filler = offset - file_inode.size;
+  if (null_filler > 0) {
+      total_written += appendData(file_inode, buf, null_filler, file_inode.size, null_filler);
+  }
+
+  // 3. Write actual data from offset (which should be file size).
+  assert(offset == file_inode.size);
+  total_written += appendData(file_inode, buf, size, offset, 0);
+
+  // 4. Write back changes to file_inode
+  this->inode_manager->set(file_inode_num, file_inode);
+  return total_written;
+}
+
+/**
+ * Appends data to the end of the file, either from buf or just NULL filler.
+ * Automatically allocates 1 extra block each time a block is needed.
+ *
+ * Invariant upon entering the function:
+ * - Offset == file size, since we are adding to the end of the file.
+ */
+size_t FileAccessManager::appendData(INode& file_inode, char *buf, size_t size, size_t offset, size_t null_filler) {
+
+  size_t total_written = 0;
+
+  // 1. Fill in the last block already allocated if it has space.
+  if (offset % Block::SIZE != 0) {
+    Block block;
+    Block::ID block_num = blockAt(file_inode, offset);
+    this->disk->get(block_num, block);
+
+    /*
+      How many bytes to write?
+      a) Write the remainder of the block.
+      b) Of course, don't write more than what was asked!
+    */
+    size_t to_write = Block::SIZE - (offset % Block::SIZE);
+    if (to_write > size) {
+      to_write = size;
+    }
+
+    // Copy the data and write to disk
+    if (null_filler == 0) {
+      memcpy(block.data  + (offset % Block::SIZE), buf, to_write);
+    } else {
+      memset(block.data + (offset % Block::SIZE), 0, to_write);
+    }
+    this->disk->set(block_num, block);
+
+    // Update offset, buf pointer, and num bytes left to write
+    offset += to_write;
+    buf += to_write;
+    size -= to_write;
+
+    file_inode.size += to_write;
+    total_written += to_write;
+  }
+
+  if (size <= 0) {
+    assert(size == 0);
+    return total_written;
+  }
+
+  // 2. Need to allocate new blocks.
+  while (size > 0) {
+
+    // Allocate the next data block
+    appendBlock(file_inode);
+
+    Block block;
+    Block::ID block_num = blockAt(file_inode, offset);
+
+    /*
+      How many bytes to write?
+      a) Write the whole block (offset should be block-aligned).
+      b) Of course, don't write more than what was asked!
+    */
+    size_t to_write = Block::SIZE;
+    if (to_write > size) {
+      to_write = size;
+    }
+
+    // Copy the data and write to disk
+    if (null_filler == 0) {
+      memcpy(block.data, buf, to_write);
+    } else {
+      memset(block.data, 0, to_write);
+    }
+    this->disk->set(block_num, block);
+
+    // Update offset, buf pointer, and num bytes left to write
+    offset += to_write;
+    buf += to_write;
+    size -= to_write;
+
+    total_written += to_write;
+  }
+  assert(size == 0);
+  return total_written;
+}
+
+/**
+ * Allocates a new data block for a file's inode.
+ * Also allocates any needed new blocks for indirect pointers.
+ */
+void FileAccessManager::appendBlock(INode& file_inode) {
+
+  size_t scale = Block::SIZE / sizeof(Block::ID);
+  size_t logical_blk_num = file_inode.blocks + 1;
+
+  if (logical_blk_num <= INode::DIRECT_POINTERS) {
+
+    // Direct block - just allocate in inode
+    file_inode.block_pointers[file_inode.blocks] = this->block_manager->reserve();
+
+  } else if (logical_blk_num <= INode::DIRECT_POINTERS + scale) {
+
+    // Single-indirect
+
+    // 1. Check if need block for the direct pointers
+    if (logical_blk_num == INode::DIRECT_POINTERS + 1) {
+      file_inode.block_pointers[INode::DIRECT_POINTERS] = this->block_manager->reserve();
+    }
+
+    // 2. Load in first level block
+    Block direct_ptrs_blk;
+    Block::ID *direct_ptrs = (Block::ID *) &direct_ptrs_blk;
+    this->disk->get(file_inode.block_pointers[INode::DIRECT_POINTERS], direct_ptrs_blk);
+
+    // 3. Allocate the direct block
+    logical_blk_num -= INode::DIRECT_POINTERS;
+    direct_ptrs[logical_blk_num - 1] = this->block_manager->reserve();
+    this->disk->set(file_inode.block_pointers[INode::DIRECT_POINTERS], direct_ptrs_blk);
+
+  } else if (logical_blk_num <= INode::DIRECT_POINTERS + scale + scale * scale) {
+
+    // Double-indirect
+
+    // 1. Check if need block for the single-indirect pointers
+    if (logical_blk_num == INode::DIRECT_POINTERS + scale + 1) {
+      file_inode.block_pointers[INode::DIRECT_POINTERS + 1] = this->block_manager->reserve();
+    }
+
+    // 2. Load in first level block
+    Block single_indirect_ptrs_blk;
+    Block::ID *single_indirect_ptrs = (Block::ID *) &single_indirect_ptrs_blk;
+    this->disk->get(file_inode.block_pointers[INode::DIRECT_POINTERS + 1], single_indirect_ptrs_blk);
+
+    // 3. Check if need block for direct pointers
+    Block::ID block_num_in_level = logical_blk_num - INode::DIRECT_POINTERS - scale;
+    if (block_num_in_level % scale == 1) {
+      single_indirect_ptrs[block_num_in_level / scale] = this->block_manager->reserve();
+      this->disk->set(file_inode.block_pointers[INode::DIRECT_POINTERS + 1], single_indirect_ptrs_blk);
+    }
+
+    // 4. Load in second level block
+    Block direct_ptrs_blk;
+    Block::ID *direct_ptrs = (Block::ID *) &direct_ptrs_blk;
+    this->disk->get(single_indirect_ptrs[block_num_in_level / scale], direct_ptrs_blk);
+
+    // 5. Allocate the direct block
+    direct_ptrs[block_num_in_level % scale - 1] = this->block_manager->reserve();
+    this->disk->set(single_indirect_ptrs[block_num_in_level / scale], direct_ptrs_blk);
+
+  } else if (logical_blk_num <= INode::DIRECT_POINTERS + scale + (scale * scale) + (scale * scale * scale)) {
+    // Triple-indirect
+
+    // 1. Check if need block for the double-indirect pointers
+    if (logical_blk_num == INode::DIRECT_POINTERS + scale + scale * scale + 1) {
+      file_inode.block_pointers[INode::DIRECT_POINTERS + 2] = this->block_manager->reserve();
+    }
+
+    // 2. Load in first level block
+    Block double_indirect_ptrs_blk;
+    Block::ID *double_indirect_ptrs = (Block::ID *) &double_indirect_ptrs_blk;
+    this->disk->get(file_inode.block_pointers[INode::DIRECT_POINTERS + 2], double_indirect_ptrs_blk);
+
+    // 3. Check if need block for single-indirect pointers
+    Block::ID block_num_in_level = logical_blk_num - INode::DIRECT_POINTERS - scale - scale * scale;
+    if (block_num_in_level % (scale * scale) == 1) {
+      double_indirect_ptrs[block_num_in_level / (scale * scale)] = this->block_manager->reserve();
+      this->disk->set(file_inode.block_pointers[INode::DIRECT_POINTERS + 2], double_indirect_ptrs_blk);
+    }
+
+    // 4. Load in second level block
+    Block single_indirect_ptrs_blk;
+    Block::ID *single_indirect_ptrs = (Block::ID *) &single_indirect_ptrs_blk;
+    this->disk->get(double_indirect_ptrs[block_num_in_level / (scale * scale)], single_indirect_ptrs_blk);
+
+    // 5. Check if need block for direct pointers
+    if (block_num_in_level % scale == 1) {
+      single_indirect_ptrs[block_num_in_level / scale] = this->block_manager->reserve();
+      this->disk->set(double_indirect_ptrs[block_num_in_level / (scale * scale)], single_indirect_ptrs_blk);
+    }
+
+    // 6. Load in third level block
+    Block direct_ptrs_blk;
+    Block::ID *direct_ptrs = (Block::ID *) &direct_ptrs_blk;
+    this->disk->get(single_indirect_ptrs[block_num_in_level / scale], direct_ptrs_blk);
+
+    // 7. Allocate direct block
+    direct_ptrs[block_num_in_level % scale - 1] = this->block_manager->reserve();
+    this->disk->set(single_indirect_ptrs[block_num_in_level / scale], direct_ptrs_blk);
+
+  } else {
+    // Can't allocate any more blocks for this file!
+    throw std::out_of_range("Offset greater than maximum file size!");
+  }
+
+  // Update the number of allocated data blocks in this inode
+  file_inode.blocks++;
+}
+
+
+
+// -------------------------------------------------------------------------------------
+
+
+size_t FileAccessManager::read(std::string path, char *buf, size_t size, size_t offset) {
+
+  // Read the file's inode and do some sanity checks
+  INode::ID file_inode_num = getINodeFromPath(path);
+  if (file_inode_num == 0) {
+    return -1; // File not found
+  }
+  INode file_inode;
+  this->inode_manager->get(file_inode_num, file_inode);
+
+  if (file_inode.type != FileType::REGULAR) {
+    return -1; // File is not a regular file
+  }
+
+  // Don't read past end of file
+  if (offset + size >= file_inode.size) {
+    size = file_inode.size - offset;
+  }
+
+  // Read until either no more data in file or size bytes read
+  size_t total_read = 0;
+  size_t cur_offset = offset;
+  while (cur_offset < offset + size) {
+
+    // Get datablock of current offset
+    Block::ID cur_block_id = blockAt(file_inode, cur_offset);
+    Block block;
+    this->disk->get(cur_block_id, block);
+
+    // Calculate bytes to read from this block
+    size_t to_read = Block::SIZE;
+    if (cur_offset % Block::SIZE != 0) {
+      to_read = Block::SIZE - (cur_offset % Block::SIZE);
+    }
+
+    if (total_read + to_read > size) {
+      to_read = size - total_read;
+    }
+
+    // Copy data from block into buffer
+    memcpy(buf, block.data, to_read);
+    cur_offset += to_read;
+    buf += to_read;
+    total_read += to_read;
+  }
+  return total_read;
+}
+
 Block::ID FileAccessManager::blockAt(const INode& inode, uint64_t offset) {
   // TODO: This function only gets existing blocks.
   // It might need reworking for writes.
-  if(offset >= inode.size) {
+  if (offset >= inode.size) {
     throw std::out_of_range("Offset greater than file size.");
   }
 
-  if(offset < INode::DIRECT_POINTERS * Block::SIZE) {
+  if (offset < INode::DIRECT_POINTERS * Block::SIZE) {
     return inode.block_pointers[offset / Block::SIZE];
   }
 
   uint64_t size  = Block::SIZE;
   uint64_t scale = Block::SIZE / sizeof(Block::ID);
-  for(int i = 0; i < 3; ++i) {
-    if(offset < size * scale) {
+  for (int i = 0; i < 3; ++i) {
+    if (offset < size * scale) {
       Block::ID bid = inode.block_pointers[INode::DIRECT_POINTERS + i];
       return indirectBlockAt(bid, offset, size);
     }
@@ -77,7 +417,6 @@ INode::ID FileAccessManager::getINodeFromPath(std::string path) {
       return 0;
     }
   }
-
   return componentLookup(cur_inode_num, path);
 }
 
@@ -97,11 +436,11 @@ INode::ID FileAccessManager::componentLookup(INode::ID did, std::string filename
 
   uint64_t offset = 0;
   uint64_t max = inode.size + Block::SIZE - 1;
-  while(offset < max) {
+  while (offset < max) {
     Block block;
     this->disk->get(blockAt(inode, offset), block);
     INode::ID iid = directLookup(&block, filename);
-    if(iid != 0) return iid;
+    if (iid != 0) return iid;
     offset += Block::SIZE;
   }
 
--- src/lib/FileAccessManager.h
+++ src/lib/FileAccessManager.h
@@ -2,25 +2,35 @@
 
 #include <cstdint>
 #include <string>
+#include <stdexcept>
+#include <cstring>
+#include <cassert>
 
 #include "Block.h"
 #include "INode.h"
+#include "BlockManager.h"
 #include "INodeManager.h"
 #include "Storage.h"
 #include "DirectoryRecord.h"
 
 class FileAccessManager {
+  BlockManager *block_manager;
   INodeManager *inode_manager;
   Storage *disk;
 public:
-  FileAccessManager(INodeManager& inode_manager, Storage &storage);
+  FileAccessManager(BlockManager &block_manager, INodeManager& inode_manager, Storage &storage);
   ~FileAccessManager();
 
   INode::ID getINodeFromPath(std::string path);
+  size_t read(std::string path, char *buffer, size_t size, size_t offset);
+  size_t write(std::string path, char *buf, size_t size, size_t offset);
 
 private:
   Block::ID blockAt(const INode& inode, uint64_t offset);
   INode::ID componentLookup(INode::ID cur_inode_num, std::string filename);
   INode::ID directLookup(Block *directory, std::string filename);
   Block::ID indirectBlockAt(Block::ID bid, uint64_t offset, uint64_t size);
+  void appendBlock(INode& file_inode);
+  size_t appendData(INode& file_inode, char *buf, size_t size, size_t offset, size_t null_filler);
+
 };
