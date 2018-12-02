d361b0f163f80604ce9a6c54d47c6ae9e15e7214
Kevin Burk <xavierholt@gmail.com>
2018-12-02 13:39:31 -0800

    UMask support and centralized UID / GID setting.

--- src/lib/Filesystem.cpp
+++ src/lib/Filesystem.cpp
@@ -94,7 +94,7 @@ int Filesystem::mount(char* program, fuse_operations* ops) {
   char f[] = "-f"; // Run in the foreground.
   char o[] = "-o"; // Other options
   char p[] = "default_permissions"; // Defer permissions checks to kernel
-  char r[] = "allow_root"; // Only allow root and user to access files
+  char r[] = "allow_other"; // All users can access the filesystem
 
   int argc = 0;
   char* argv[12] = {0};
--- src/lib/INode.cpp
+++ src/lib/INode.cpp
@@ -2,14 +2,19 @@
 
 #include <cstring>
 #include <ctime>
+#include <fuse.h>
 
 INode::INode() {
   std::memset(this, 0, sizeof(INode));
 }
 
 INode::INode(FileType type, uint16_t mode, uint64_t dev): INode() {
+  fuse_context* context = fuse_get_context();
+
   this->type   = type;
-  this->mode   = mode;
+  this->mode   = mode & ~context->umask;
+  this->uid    = context->uid;
+  this->gid    = context->gid;
   this->atime  = time(NULL);
   this->ctime  = this->atime;
   this->mtime  = this->atime;
